bcb5b2fa5d8e8723d761c16ff318e41e21cd2059
anshul <anshularya11@gmail.com>
2022-02-02 11:29:43 +0530

    updates in project

--- src/main/java/com/anshul/CoronavirusTracker/CoronavirusTrackerApplication.java
+++ src/main/java/com/anshul/CoronavirusTracker/CoronavirusTrackerApplication.java
@@ -2,8 +2,10 @@ package com.anshul.CoronavirusTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronavirusTrackerApplication {
 
 	public static void main(String[] args) {
